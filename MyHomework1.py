3c5c01f232b3309a05874971a9748ec984f46c49
KirillVentrue <109892022+KirillVentrue@users.noreply.github.com>
2022-09-26 18:56:07 +0500

    Update MyHomework1.py

--- MyHomework1.py
+++ MyHomework1.py
@@ -1,97 +1,95 @@
-# Задача 1. Напишите программу, которая принимает на вход
-# цифру, обозначающую день недели, и проверяет,
-# является ли этот день выходным.
-# *Пример: *
-# - 6 -> да
-# - 7 -> да
-# - 1 -> нет
-# -
+ Задача 1. Напишите программу, которая принимает на вход
+ цифру, обозначающую день недели, и проверяет,
+ является ли этот день выходным.
+ *Пример: *
+ - 6 -> да
+ - 7 -> да
+ - 1 -> нет
+ -
 
-# def InputNumbers(inputText):
-#     is_OK = False
-#     while not is_OK:
-#         try:
-#             number = int(input(f"{inputText}"))
-#             is_OK = True
-#         except ValueError:
-#             print("Это не число!")
-#     return number
+ def InputNumbers(inputText):
+     is_OK = False
+     while not is_OK:
+         try:
+             number = int(input(f"{inputText}"))
+             is_OK = True
+         except ValueError:
+             print("Это не число!")
+     return number
 
-# def checkNumber(num):
-#     if 6 <= num <= 7:
-#         print("Этот день является выходным")
-#     elif 0 < num < 6:
-#         print("Этот день не является выходным")
-#     else:
-#         print("число вне пределов 7 дней")
+ def checkNumber(num):
+     if 6 <= num <= 7:
+         print("Этот день является выходным")
+     elif 0 < num < 6:
+         print("Этот день не является выходным")
+     else:
+         print("число вне пределов 7 дней")
 
-# num = InputNumbers("Введите цифру обозначающую день недели: ")
-# checkNumber(num)
+ num = InputNumbers("Введите цифру обозначающую день недели: ")
+ checkNumber(num)
 
-# Задача 2. Напишите программу для проверки истинности
-# утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех
-# значений предикат.
+ Задача 2. Напишите программу для проверки истинности
+ утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех
+ значений предикат.
 
-# def inputNumbers(x):
-#     xyz = ["X", "Y", "Z"]
-#     a = []
-#     for i in range(x):
-#         a.append(input(f"Введите значение {xyz[i]}: "))
-#     return a
+ def inputNumbers(x):
+     xyz = ["X", "Y", "Z"]
+     a = []
+     for i in range(x):
+         a.append(input(f"Введите значение {xyz[i]}: "))
+     return a
 
-# def checkPredicate(x):
-#     left = not (x[0] or x[1] or x[2])
-#     right = not x[0] and not x[1] and not x[2]
-#     result = left == right
-#     return result
+ def checkPredicate(x):
+     left = not (x[0] or x[1] or x[2])
+     right = not x[0] and not x[1] and not x[2]
+     result = left == right
+     return result
 
-# statement = inputNumbers(3)
+ statement = inputNumbers(3)
 
-# if checkPredicate(statement) == True:
-#     print(f"Утверждение истинно")
-# else:
-#     print(f"Утверждение ложно")
+ if checkPredicate(statement) == True:
+     print(f"Утверждение истинно")
+ else:
+     print(f"Утверждение ложно")
 
-# Задача 3. Напишите программу, которая принимает на вход
-# координаты точки(X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
-# номер четверти плоскости, в которой находится эта
-# точка(или на какой оси она находится).
+ Задача 3. Напишите программу, которая принимает на вход
+ координаты точки(X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+ номер четверти плоскости, в которой находится эта
+ точка(или на какой оси она находится).
 
-# *Пример: *
-# - x = 34
-# y = -30 -> 4
-# - x = 2
-# y = 4 -> 1
-# - x = -34
-# y = -30 -> 3
+ *Пример: *
+ - x = 34
+ y = -30 -> 4
+ - x = 2
+ y = 4 -> 1
+ - x = -34
+ y = -30 -> 3
 
-# def inputKoord(x):
-#     a = [0] * x
-#     for i in range(x):
-#         is_OK = False
-#         while not is_OK:
-#             try:
-#                 number = float(input(f"Введите {i+1} координату: "))
-#                 a[i] = number
-#                 is_OK = True
-#                 if a[i] == 0:
-#                     is_OK = False
-#                     print("Координата не должно быть равна 0 ")
-#             except ValueError:
-#                 print("Ты ошибся. Вводить надо числа!")
-#     return a
+ def inputKoord(x):
+     a = [0] * x
+     for i in range(x):
+         is_OK = False
+         while not is_OK:
+             try:
+                 number = float(input(f"Введите {i+1} координату: "))
+                 a[i] = number
+                 is_OK = True
+                 if a[i] == 0:
+                     is_OK = False
+                     print("Координата не должно быть равна 0 ")
+             except ValueError:
+                 print("Ты ошибся. Вводить надо числа!")
+     return a
 
+ def checkCoordinates(xy):
+     text = 4
+     if xy[0] > 0 and xy[1] > 0:
+         text = 1
+     elif xy[0] < 0 and xy[1] > 0:
+         text = 2
+     elif xy[0] < 0 and xy[1] < 0:
+         text = 3
+     print(f"Точка находится в {text} четверти плоскости")
 
-# def checkCoordinates(xy):
-#     text = 4
-#     if xy[0] > 0 and xy[1] > 0:
-#         text = 1
-#     elif xy[0] < 0 and xy[1] > 0:
-#         text = 2
-#     elif xy[0] < 0 and xy[1] < 0:
-#         text = 3
-#     print(f"Точка находится в {text} четверти плоскости")
-
-
-# koordinate = inputKoord(2)
-# checkCoordinates(koordinate)
+ koordinate = inputKoord(2)
+ checkCoordinates(koordinate)
